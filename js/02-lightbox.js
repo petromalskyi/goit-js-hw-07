82e94d99e51ea5cb7a8da745ad241b8d0f1815dc
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-03-06 18:15:57 +0200

    finished hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,48 +3,12 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-// const divEl = document.querySelector('.gallery');
-// const cardsItem = createElement(galleryItems);
-
-// //  divEl.classList.add('gallery');
-// divEl.insertAdjacentHTML('beforeend', cardsItem);
-
-// function createElement(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//     <li><a class="gallery__item" href="${original}" alt="${description}">
-//     <img class="gallery__image" src="${preview}" alt="${description}"/>
-//     </a></li>`;
-//     })
-//     .join('');
-// }
-
-// divEl.addEventListener('click', clickOpenModal);
-
-// function clickOpenModal(event) {
-//   event.preventDefault();
-
-//   if (!event.target.classList.contains('gallery__image')) {
-//     return;
-//   }
-
-//     const clickOpenModalEl = event.target;
-//   const indexGalleryItems = galleryItems.findIndex(
-//     option => option.description === clickOpenModalEl.alt,
-//   );
-
-//   let gallery = new SimpleLightbox('.gallery a');
-
-//     gallery.on('show.simplelightbox', function () {
-//        captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
-//     });
-
 const galleryContainer = document.querySelector('.gallery');
 galleryContainer.insertAdjacentHTML(
   'beforeend',
   createGalleryItemsMarkup(galleryItems),
 );
+
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -66,4 +30,4 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-//}
+
